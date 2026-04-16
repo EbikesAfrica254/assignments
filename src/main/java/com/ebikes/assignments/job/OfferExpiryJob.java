9305d811b9f55648ccbd6bc80c10ba6c3e0503fd
mango-habanero <main@mango-habanero.dev>
2026-04-17 01:21:50 +0300

    refactor(jobs): simplify OfferExpiryJob by delegating expired offer processing to OfferExpiryService

--- src/main/java/com/ebikes/assignments/job/OfferExpiryJob.java
+++ src/main/java/com/ebikes/assignments/job/OfferExpiryJob.java
@@ -1,17 +1,9 @@
 package com.ebikes.assignments.job;
 
-import java.time.OffsetDateTime;
-import java.time.ZoneOffset;
-import java.util.List;
-
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import com.ebikes.assignments.database.entities.AssignmentOffer;
-import com.ebikes.assignments.database.repositories.AssignmentOfferRepository;
-import com.ebikes.assignments.enums.OfferStatus;
-import com.ebikes.assignments.services.assignments.OfferExpiryService;
-import com.ebikes.assignments.support.context.ExecutionContext;
+import com.ebikes.assignments.services.offers.OfferExpiryService;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -21,26 +13,10 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class OfferExpiryJob {
 
-  private final AssignmentOfferRepository assignmentOfferRepository;
   private final OfferExpiryService offerExpiryService;
 
   @Scheduled(fixedDelayString = "${assignments.expiry.poll-interval-ms:15000}")
   public void processExpiredOffers() {
-    List<AssignmentOffer> expired =
-        assignmentOfferRepository.findByStatusAndExpiresAtBefore(
-            OfferStatus.CREATED, OffsetDateTime.now(ZoneOffset.UTC));
-
-    if (expired.isEmpty()) {
-      return;
-    }
-
-    log.debug("Processing {} expired offers", expired.size());
-
-    ExecutionContext.setSystem();
-    try {
-      expired.forEach(offerExpiryService::processOffer);
-    } finally {
-      ExecutionContext.clear();
-    }
+    offerExpiryService.processExpired();
   }
 }
